f95538642c6029c7b566ac35c5cdd26b599f45b8
mubuntu <patrick.a.frison@gmail.com>
2018-05-30 01:00:51 +0200

    changed ComponentScan to scan based on classes in base-package

--- chapter-2/src/main/java/edu/springinaction/chapter2/soundsystem/config/CDPlayerConfig.java
+++ chapter-2/src/main/java/edu/springinaction/chapter2/soundsystem/config/CDPlayerConfig.java
@@ -1,5 +1,6 @@
 package edu.springinaction.chapter2.soundsystem.config;
 
+import edu.springinaction.chapter2.soundsystem.CompactDisc;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
  * Time: 00:28
  **/
 @Configuration
-@ComponentScan(basePackages = "edu.springinaction.chapter2.soundsystem")
+// will search base package based on classed given:
+@ComponentScan(basePackageClasses = CompactDisc.class)
 public class CDPlayerConfig {
 }
